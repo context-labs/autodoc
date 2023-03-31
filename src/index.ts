3e7f040d19816e612fa619c378a74eb7c6ffc7ff
Sam Heutmaker <samheutmaker@gmail.com>
2023-03-31 14:32:26 -0600

    v0.0.9

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ import { userConfigFilePath } from './const.js';
 
 const program = new Command();
 program.description('Autodoc CLI Tool');
-program.version('0.0.8');
+program.version('0.0.9');
 
 program
   .command('init')
